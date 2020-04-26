5348f85799f9ea95e6cbeaedd6d9bc9a9bd49806
janeluck <janeluck158@gmail.com>
2020-04-26 19:37:08 +0800

    参数说明和容错处理

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+/**
+ * Extract the url that can be opened by the browser from the text.
+ * 提取符合浏览器打开规则的url
+ *
+ * @param {string} text 输入的文本
+ * @return {Array} 从输入文本中提取到的，符合浏览器打开规则的url数组
+ */
+
 module.exports = function (text) {
     let str = text;
     const result = [];
+    if (typeof str !== 'string') {
+        return result;
+    }
+
     const rules = [
         // 带有协议的类型（应用自定义eg: evernote://x.x）
         /[a-z]+:\/\/[^\s]+/ig,

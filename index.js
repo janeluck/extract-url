788a75bea25a3b13d787e4f1c1e40ed8ae357269
janeluck <janeluck158@gmail.com>
2020-04-26 17:32:10 +0800

    ok

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-module.exports = function (text, callback) {
-
+module.exports = function (text) {
+    let str = text;
+    const result = [];
     const rules = [
         // 带有协议的类型（应用自定义eg: evernote://x.x）
-        /[a-zA-Z]+:\/\/[^\s]+/,
+        /[a-zA-Z]+:\/\/[^\s]+/g,
 
         // 不带协议，以顶级域名结尾
         // todo 补充顶级域名
-        /([a-zA-Z0-9]+\.)+(com|cn|jp)[^\s]+/,
+        /([a-zA-Z0-9]+\.)+(com|cn|jp)\/?[^\s]*/g,
 
         // ip形式
         // ipV4 (1~255).(0~255).(0~255).(0.255)
 
     ];
-    return true
 
+    rules.forEach(function (rule) {
+        str = str.replace(rule, function (oldStr, newStr) {
+            result.push(oldStr);
+            return '';
+        })
+    });
+
+    return result;
 };
--- test/customProtocol.js
+++ test/customProtocol.js
@@ -1,10 +1,24 @@
 const assert = require('assert');
 const extractUrl = require('../index');
 describe('Simple Math Test', () => {
-    it('should return true', () => {
-        assert.equal(extractUrl('evernote://x.y'), true);
+    const str1 = '哈哈：evernote://x.y 213132',
+        str2 = '哈哈：www.io.com',
+        str3 = '哈哈：www.io.com/hhd/#22?id=2&name=jane',
+        all = str1 + '\n' + str2 + '\n' + str3;
+    it(`custom protocol: ${str1}`, () => {
+        assert.deepEqual(extractUrl(str1), ['evernote://x.y']);
     });
-    it('should return 9', () => {
-        assert.equal(3 * 3, 9);
+    it(`has no protocol 1: ${str2}`, () => {
+        assert.deepEqual(extractUrl(str2), ['www.io.com']);
     });
+
+    it(`has no protocol 2: ${str3}`, () => {
+        assert.deepEqual(extractUrl(str3), ['www.io.com/hhd/#22?id=2&name=jane']);
+    });
+
+
+    it(`all: ${all}`, () => {
+        assert.deepEqual(extractUrl(all), ['evernote://x.y', 'www.io.com', 'www.io.com/hhd/#22?id=2&name=jane']);
+    });
+
 });

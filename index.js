795bc7f9bace0bc2d22092f731b710a34f388f26
janeluck <janeluck158@gmail.com>
2020-04-26 19:15:30 +0800

    top domain

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ module.exports = function (text) {
     const result = [];
     const rules = [
         // 带有协议的类型（应用自定义eg: evernote://x.x）
-        /[a-zA-Z]+:\/\/[^\s]+/g,
+        /[a-z]+:\/\/[^\s]+/ig,
 
         // 不带协议，以顶级域名结尾
         // todo 补充顶级域名
-        /([a-zA-Z0-9]+\.)+(com|cn|jp)\/?[^\s]*/g,
+        /([a-z0-9]+\.)+(com|org|net|edu|gov|mil)((\/[^\s]*)|(\/?))/ig,
 
         // ip形式
         // ipV4 (1~255).(0~255).(0~255).(0.255)
